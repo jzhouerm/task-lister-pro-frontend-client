51af6143718bca60c99b64b24d161bd00c64e760
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2020-11-16 23:34:02 -0500

    11-16-20 index.js comments and notes added

--- src/index.js
+++ src/index.js
@@ -513,24 +513,24 @@ createCalendar(calendarDiv, 2020, month);
 
 })//end of DOMContentLoaded
 
-// /*
-// To-do:
+/*
+To-do:
 
-// - re-render the tasks for the same day after deleting a task
-// - fix the calendar to view different months
-// - add new task to iCal
-// - format form
-// - set validations
+- re-render the tasks for the same day after deleting a task
+- fix the calendar to view different months
+- add new task to iCal
+- format form
+- set validations
 
-// ***Update month in createCalendar function and update seeds to reflect current month
+***Update month in createCalendar function and update seeds to reflect current month
 
-// Backend
-// 1) run rails db:migrate
-// 2) run rails db:seed
-// 3) run rails s
+Backend
+1) run rails db:migrate
+2) run rails db:seed
+3) run rails s
 
-// Front end
-// 4) open index.js
+Front end
+4) open index.js
 
-// */
+*/
 
