ac74e5812d34371ffe7a62a9b16b7f224876e992
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2020-11-29 23:40:39 -0500

    11-29-20 index.html - updated so timer reflects pomodoros based on task assigned

--- src/index.js
+++ src/index.js
@@ -203,7 +203,6 @@ document.addEventListener("DOMContentLoaded", e => {
             threshold: ALERT_THRESHOLD
           }
         };
-        //upon click on taskLi, the time is set to taskpom * 1500
         let TIME_LIMIT 
         let timePassed = 0;
         let timeLeft = TIME_LIMIT;
